528aa6d03953e257b570e532506355e932516c9c
Mahdi-dmr1 <damirchi9904@gmail.com>
2024-07-11 03:29:13 +0430

    Zustand store

--- store/index.ts
+++ store/index.ts
@@ -5,30 +5,25 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import { TodoState, Task } from "../types";
 
 export const useTodoStore = create<TodoState>()(
-	persist(
-		(set) => ({
-			tasks: [],
-			addTask: (title, description) =>
-				set((state) => ({
-					tasks: [
-						...state.tasks,
-						{ id: Date.now(), title, description, done: false },
-					],
-				})),
-			removeTask: (id) =>
-				set((state) => ({
-					tasks: state.tasks.filter((task) => task.id !== id),
-				})),
-			toggleTaskDone: (id) =>
-				set((state) => ({
-					tasks: state.tasks.map((task) =>
-						task.id === id ? { ...task, done: !task.done } : task
-					),
-				})),
-		}),
-		{
-			name: "todo-storage",
-			storage: createJSONStorage(() => localStorage),
-		}
-	)
+    persist(
+        (set) => ({
+            tasks: [],
+            addTask: (title, description) =>
+                set((state) => ({
+                    tasks: [...state.tasks, { id: Date.now(), title, description, done: false }],
+                })),
+            removeTask: (id) =>
+                set((state) => ({
+                    tasks: state.tasks.filter((task) => task.id !== id),
+                })),
+            toggleTaskDone: (id) =>
+                set((state) => ({
+                    tasks: state.tasks.map((task) => (task.id === id ? { ...task, done: !task.done } : task)),
+                })),
+        }),
+        {
+            name: "todo-storage",
+            storage: createJSONStorage(() => localStorage),
+        },
+    ),
 );
